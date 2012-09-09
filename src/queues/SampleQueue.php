70a6b0f346b43958033c84e1b049fa7b3e8a8aeb
Michael Cheng <michael.cheng@mig33global.com>
2012-09-09 21:22:23 +0800

    More methods for SampleQueue.

--- src/queues/SampleQueue.php
+++ src/queues/SampleQueue.php
@@ -2,9 +2,32 @@
 require __DIR__ . '/../vendor/autoload.php';
 class SampleQueue extends PHPQueue\JobQueue
 {
+	private $jobs = array();
+	public function addJob($newJob)
+	{
+		parent::addJob($newJob);
+		array_unshift($this->jobs, $newJob);
+		return true;
+	}
+
 	public function getJob()
 	{
 		parent::getJob();
+		if ( empty($this->jobs) )
+		{
+			throw new \PHPQueue\Exception("No more jobs.");
+		}
+		$jobData = array_pop($this->jobs);
+		$nextJob = new \PHPQueue\Job();
+		$nextJob->data = $jobData;
+		$nextJob->worker = 'Sample';
+		return $nextJob;
+	}
+
+	public function getQueueSize()
+	{
+		parent::getQueueSize();
+		return count($this->jobs);
 	}
 }
 ?>
