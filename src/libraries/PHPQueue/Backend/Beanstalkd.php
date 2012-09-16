331357700d098d5ddddeb43aad05e0549db4afc4
Michael Cheng <michael.cheng@mig33global.com>
2012-09-16 18:58:38 +0800

    Add Beanstalkd backend

---
+++ src/libraries/PHPQueue/Backend/Beanstalkd.php
@@ -0,0 +1,68 @@
+<?php
+namespace PHPQueue\Backend;
+class Beanstalkd extends Base
+{
+	public $server_uri;
+	public $tube;
+
+	public function __construct($options=array())
+	{
+		parent::__construct();
+		if (!empty($options['server']))
+		{
+			$this->server_uri = $options['server'];
+		}
+		if (!empty($options['tube']))
+		{
+			$this->tube = $options['tube'];
+		}
+	}
+
+	public function connect()
+	{
+		$this->connection = new \Pheanstalk($this->server_uri);
+	}
+
+	public function add($data=array())
+	{
+		$this->beforeAdd();
+		return $this->connection->useTube($this->tube)->put(json_encode($data));
+	}
+
+	public function get()
+	{
+		$this->beforeGet();
+		$newJob = $this->connection->watch($this->tube)->reserve();
+		$this->last_job = $newJob;
+		$this->last_job_id = $newJob->getId();
+		$this->afterGet();
+		return $newJob;
+	}
+
+	public function clear($jobId=null)
+	{
+		$this->beforeClear();
+		if (empty($this->open_items[$jobId]))
+		{
+			throw new \PHPQueue\Exception("Job was not previously retrieved.");
+		}
+		$theJob = $this->open_items[$jobId];
+		$this->connection->delete($theJob);
+		$this->last_job_id = $jobId;
+		$this->afterClearRelease();
+	}
+
+	public function release($jobId=null)
+	{
+		$this->beforeRelease();
+		if (empty($this->open_items[$jobId]))
+		{
+			throw new \PHPQueue\Exception("Job was not previously retrieved.");
+		}
+		$theJob = $this->open_items[$jobId];
+		$this->connection->release($theJob);
+		$this->last_job_id = $jobId;
+		$this->afterClearRelease();
+	}
+}
+?>
