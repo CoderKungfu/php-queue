af3d801b5bda2e31c676c6a3beaadd0e877fcbc3
Michael Cheng <mcheng.work@gmail.com>
2012-11-19 22:58:51 +0800

    Update path of config in demo and PHPUnit bootstrap file.

--- src/demo/config.php
+++ src/demo/config.php
@@ -1,5 +1,5 @@
 <?php
-require_once dirname(__DIR__) . '/vendor/autoload.php';
+require_once dirname(dirname(__DIR__)) . '/vendor/autoload.php';
 PHPQueue\Base::$queuePath = __DIR__ . '/queues/';
 PHPQueue\Base::$workerPath = __DIR__ . '/workers/';
 ?>
--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,3 +1,5 @@
 <?php
-require_once dirname(__DIR__) . '/src/vendor/autoload.php';
+require_once dirname(__DIR__) . '/vendor/autoload.php';
+PHPQueue\Base::$queuePath = dirname(__DIR__) . '/src/demo/queues/';
+PHPQueue\Base::$workerPath = dirname(__DIR__) . '/src/demo/workers/';
 ?>
