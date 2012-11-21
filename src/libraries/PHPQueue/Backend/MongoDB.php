ca9142a0097471e6aa6e29ca5f4d45337971e096
Michael Cheng <mcheng.work@gmail.com>
2012-11-22 01:57:07 +0800

    Add MongoDB support. Plus tests.

---
+++ src/libraries/PHPQueue/Backend/MongoDB.php
@@ -0,0 +1,114 @@
+<?php
+namespace PHPQueue\Backend;
+class MongoDB extends Base
+{
+	public $server_uri;
+	public $db_name;
+	public $collection_name;
+	public $mongo_options = array("connect"=>true);
+
+	public function __construct($options=array())
+	{
+		parent::__construct();
+		if (!empty($options['server']))
+		{
+			$this->server_uri = $options['server'];
+		}
+		if (!empty($options['db']) && is_string($options['db']))
+		{
+			$this->db_name = $options['db'];
+		}
+		if (!empty($options['collection']) && is_string($options['collection']))
+		{
+			$this->collection_name = $options['collection'];
+		}
+		if (!empty($options['mongo_options']) && is_array($options['mongo_options']))
+		{
+			$this->mongo_options = array_merge($this->mongo_options, $options['mongo_options']);
+		}
+	}
+
+	public function connect()
+	{
+		if (empty($this->server_uri))
+		{
+			throw new \PHPQueue\Exception("No server specified");
+		}
+		$this->connection = new \Mongo($this->server_uri, $this->mongo_options);
+	}
+
+	public function getDB()
+	{
+		if (empty($this->db_name) || !is_string($this->db_name))
+		{
+			throw new \PHPQueue\Exception("DB is invalid.");
+		}
+		$db = $this->db_name;
+		return $this->getConnection()->$db;
+	}
+
+	public function getCollection()
+	{
+		if (empty($this->collection_name) || !is_string($this->collection_name))
+		{
+			throw new \PHPQueue\Exception("Collection is invalid.");
+		}
+		$db = $this->getDB();
+		$collection = $this->collection_name;
+		return $db->$collection;
+	}
+
+	public function add($data=null, $key=null)
+	{
+		if (empty($data) || !is_array($data))
+		{
+			throw new \PHPQueue\Exception("No data.");
+		}
+		if (!isset($data['_id']))
+		{
+			$data['_id'] = !empty($key) ? $key : uniqid();
+		}
+		$this->beforeAdd();
+		$the_collection = $this->getCollection();
+		$status = $the_collection->insert($data);
+		if (!$status)
+		{
+			throw new \PHPQueue\Exception("Unable to save data.");
+		}
+		$this->last_job_id = $data['_id'];
+		return $status;
+	}
+
+	/**
+	 * @param string $key
+	 * @return mixed
+	 */
+	public function get($key=null)
+	{
+		$this->beforeGet();
+		$cursor = $this->getCollection()->find(array('_id' => $key));
+		if ($cursor->count() < 1)
+		{
+			return null;
+		}
+		$cursor->next();
+		$data = $cursor->current();
+		unset($data['_id']);
+		return $data;
+	}
+
+	public function clear($key=null)
+	{
+		$this->beforeClear();
+		$data = $this->get($key);
+		if (is_null($data))
+		{
+			throw new \PHPQueue\Exception("Record not found.");
+		}
+		$response = $this->getCollection()->remove(array('_id' => $key));
+		var_dump($response);
+		$this->last_job_id = $key;
+		return true;
+	}
+}
+?>
---
+++ tests/PHPQueue/Backend/MongoDBTest.php
@@ -0,0 +1,69 @@
+<?php
+class MongoDBTest extends PHPUnit_Framework_TestCase
+{
+	private $object;
+	private $ids = array();
+
+	public function setUp()
+	{
+		parent::setUp();
+		$options = array(
+			  'server' => 'mongodb://localhost'
+			, 'db'  => 'testdb'
+			, 'collection' => 'things'
+		);
+		$this->object = new PHPQueue\Backend\MongoDB($options);
+	}
+
+	public function testAddGet()
+    {
+		$this->object->getDB()->drop();
+
+		$key = 'A0001';
+		$data1 = array('name' => 'Michael');
+		$result = $this->object->add($data1, $key);
+		$this->assertTrue($result);
+
+		$result = $this->object->get($key);
+		$this->assertNotEmpty($result);
+		$this->assertEquals($data1, $result);
+
+		$data2 = array('1','Willy','Wonka');
+		$result = $this->object->add($data2);
+		$this->assertTrue($result);
+
+		$last_id = $this->object->last_job_id;
+
+		$result = $this->object->get($last_id);
+		$this->assertNotEmpty($result);
+		$this->assertEquals($data2, $result);
+	}
+
+	/**
+	 * @depends testAddGet
+	 */
+	public function testClear()
+    {
+		try
+		{
+			$jobId = 'xxx';
+			$result = $this->object->clear($jobId);
+		}
+		catch (\PHPQueue\Exception $ex)
+		{
+			$this->assertTrue(true);
+		}
+		catch (\Exception $ex)
+		{
+			$this->fail('Should not be able to delete.');
+		}
+
+		$jobId = 'A0001';
+		$result = $this->object->clear($jobId);
+		$this->assertTrue($result);
+
+		$result = $this->object->get($jobId);
+		$this->assertNull($result);
+	}
+}
+?>
