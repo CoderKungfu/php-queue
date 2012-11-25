23ca3a627d3d87eda52fd12b95b2acb4811907e4
Michael Cheng <mcheng.work@gmail.com>
2012-11-25 16:26:56 +0800

    Specify name of queue inside the class instead of adding it on instantiation time.

--- src/demo/runners/SampleRunner.php
+++ src/demo/runners/SampleRunner.php
@@ -1,6 +1,9 @@
 <?php
 require_once dirname(__DIR__) . '/config.php';
-class SampleRunner extends PHPQueue\Runner{}
-$runner = new SampleRunner('Sample');
+class SampleRunner extends PHPQueue\Runner
+{
+	public $queue_name = 'Sample';
+}
+$runner = new SampleRunner();
 $runner->run();
 ?>
