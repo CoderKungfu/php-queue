54386d1af2f8a96bb2f28797392af1e764e7ebc6
Michael Cheng <mcheng.work@gmail.com>
2012-12-17 21:55:49 +0800

    Test case for WindowsAzureBlob class.

---
+++ test/PHPQueue/Backend/WindowsAzureBlobTest.php
@@ -0,0 +1,94 @@
+<?php
+namespace PHPQueue\Backend;
+class WindowsAzureBlobTest extends \PHPUnit_Framework_TestCase
+{
+    private $object;
+
+    public function setUp()
+    {
+        parent::setUp();
+        if (!class_exists('\WindowsAzure\Common\ServicesBuilder'))
+        {
+            $this->markTestSkipped('Windows Azure not installed');
+        }
+        else
+        {
+            $options = array(
+                  'connection_string' => 'DefaultEndpointsProtocol=https;AccountName=noobsgblob;AccountKey=WHkYwMCHYFMB1EHu061XlD11XS7v0gzKWcYKh4s5YTTioWpyYIVOki2KYki42gekpaVLmKN9WaYc3elyvh/qpQ=='
+            );
+            $this->object = new WindowsAzureBlob($options);
+        }
+    }
+
+    public function testManageContainers()
+    {
+        $container_name = 'test'.time();
+
+        $result = $this->object->listContainers();
+        $this->assertEmpty($result);
+
+        $result = $this->object->createContainer($container_name);
+        $this->assertTrue($result);
+
+        $result = $this->object->listContainers();
+        $this->assertEquals(1, count($result));
+
+        $result = $this->object->deleteContainer($container_name);
+        $this->assertTrue($result);
+
+        $result = $this->object->listContainers();
+        $this->assertEmpty($result);
+    }
+
+    public function testAdd()
+    {
+        sleep(1);
+        $container_name = 'testimg';
+        $result = $this->object->createContainer($container_name);
+        $this->assertTrue($result);
+
+        $this->object->setContainer($container_name);
+        $file = __DIR__ . '/cc_logo.jpg';
+        $result = $this->object->put('image.jpg', $file);
+        $this->assertTrue($result);
+
+        $result = $this->object->listFiles();
+        $this->assertNotEmpty($result);
+    }
+
+    /**
+     * @depends testAdd
+     */
+    public function testGet()
+    {
+        $container_name = 'testimg';
+        $this->object->setContainer($container_name);
+        $result = $this->object->fetch('image.jpg');
+        $this->assertNotEmpty($result);
+    }
+
+    /**
+     * @depends testAdd
+     */
+    public function testClear()
+    {
+        $container_name = 'testimg';
+        $this->object->setContainer($container_name);
+        $result = $this->object->clear('image.jpg');
+        $this->assertTrue($result);
+
+        try
+        {
+            $fake_filename = 'xxx';
+            $this->object->clear($fake_filename);
+            $this->fail("Should not be able to delete.");
+        }
+        catch(\Exception $ex)
+        {
+            $this->assertTrue(true);
+        }
+
+        $result = $this->object->deleteContainer($container_name);
+        $this->assertTrue($result);
+    }
+}
