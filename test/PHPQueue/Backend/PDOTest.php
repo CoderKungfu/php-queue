ff3eb61b7992c810d7b8ee9659b61cfba8fe39f4
Michael Cheng <mcheng.work@gmail.com>
2012-12-26 20:58:58 +0800

    PDO class test.

---
+++ test/PHPQueue/Backend/PDOTest.php
@@ -0,0 +1,68 @@
+<?php
+namespace PHPQueue\Backend;
+class PDOTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var PDO
+     */
+    private $object;
+
+    public function setUp()
+    {
+        parent::setUp();
+        if (!class_exists('\PDO'))
+        {
+            $this->markTestSkipped('PDO extension is not installed');
+        }
+        else
+        {
+            $options = array(
+                  'connection_string' => 'mysql:host=localhost;dbname=phpqueuetest'
+                , 'db_user'           => 'root'
+                , 'db_password'       => 'media1'
+                , 'db_table'          => 'pdotest'
+            );
+            $this->object = new PDO($options);
+        }
+    }
+
+    public function testAddGet()
+    {
+        $result = $this->object->createTable('pdotest');
+        $this->assertTrue($result);
+
+        $this->object->clearAll();
+
+        $data1 = array('2','Boo','Moeow');
+        $result = $this->object->add($data1);
+        $this->assertTrue($result);
+
+        $this->assertEquals(1, $this->object->last_job_id);
+
+        $data2 = array('1','Willy','Wonka');
+        $result = $this->object->add($data2);
+        $this->assertTrue($result);
+
+        $last_id = $this->object->last_job_id;
+
+        $result = $this->object->get($last_id);
+        $this->assertNotEmpty($result);
+        $this->assertEquals($data2, $result);
+    }
+
+    /**
+     * @depends testAddGet
+     */
+    public function testClear()
+    {
+        $jobId = 1;
+        $result = $this->object->clear($jobId);
+        $this->assertTrue($result);
+
+        $result = $this->object->get($jobId);
+        $this->assertNull($result);
+
+        $result = $this->object->deleteTable('pdotest');
+        $this->assertTrue($result);
+    }
+}
