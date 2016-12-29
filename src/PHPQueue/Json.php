0d33b8d46f5418bbe9f0a7cd6fe146ba1cb303d1
Elliott Eggleston <ejegg@ejegg.com>
2016-12-28 19:23:30 -0500

    Decoding null to null is not an error
    Quit throwing exceptions when the queue is empty.
    
    Change-Id: Ie5479c81c0a6138e1cad416b40d3edabffb1e9c4

--- src/PHPQueue/Json.php
+++ src/PHPQueue/Json.php
@@ -5,6 +5,9 @@ use PHPQueue\Exception\JsonException;
 
 class Json {
     public static function safe_decode( $text ) {
+        if ( $text === null ) {
+            return null;
+        }
         $data = json_decode($text, true);
         if ( $data === null ) {
             throw new JsonException("JSON could not be decoded: '{$text}'");
